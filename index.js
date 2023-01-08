464e6b2f0f3613c715b042c8aa4ba73b787c92e4
TheFatPanda97 <shawnhu97@gmail.com>
2023-01-08 15:41:54 -0500

    finished script

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const axios = require('axios');
 
-console.log(ACCESS_TOKEN);
+const VALUE = process.env.VALUE;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const DNS_ZONE = process.env.DNS_ZONE;
+const HOSTNAME = process.env.HOSTNAME;
+const DOMAIN = process.env.DOMAIN;
+
+const instance = axios.create({
+  baseURL: `https://api.netlify.com/api/v1/dns_zones/${DNS_ZONE}/dns_records`,
+  timeout: 5000,
+  headers: {
+    Authorization: `Bearer ${ACCESS_TOKEN}`,
+  },
+});
+
+(async () => {
+  try {
+    const { data: all_records } = await instance.get();
+    const service_record = all_records.find(
+      (record) => record.hostname === `${HOSTNAME}.${DOMAIN}`,
+    );
+
+    // there is already a A record, delete it
+    if (service_record) {
+      await instance.delete(`/${service_record.id}`);
+    }
+
+    // add a new A record
+    await instance.post('', {
+      type: 'A',
+      hostname: HOSTNAME,
+      value: VALUE,
+    });
+
+    console.log("Success!!");
+  } catch (error) {
+    console.error(error);
+  }
+})();
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 export $(grep -v '^#' .env | xargs) # magic to export env variables in .env
+export VALUE=$(curl -s ifconfig.me) # in my case I am adding a record for my public ip but you can of course change this
 node index.js
