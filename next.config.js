c24b081d14bbd9b5bbbe01a652b3c2f4fa4f7928
TheFatPanda97 <shawnhu97@gmail.com>
2023-08-18 12:58:14 -0400

    fixed cors issues

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, options) => {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.(graphql|gql)/,
       exclude: /node_modules/,
@@ -8,6 +8,24 @@ const nextConfig = {
     });
 
     return config;
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
   },
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,25 @@
 import '../scss/global.scss';
 import type { AppProps } from 'next/app';
+import { useState, useEffect } from 'react';
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
-  cache: new InMemoryCache(),
-});
-
+import { ApolloClient, ApolloProvider, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const [client, setClient] = useState<ApolloClient<NormalizedCacheObject>>();
+
+  useEffect(() => {
+    setClient(
+      new ApolloClient({
+        uri: `${window.location.origin}/api/graphql`,
+        cache: new InMemoryCache(),
+      }),
+    );
+  }, []);
+
+  if (!client) {
+    return <></>;
+  }
+
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
