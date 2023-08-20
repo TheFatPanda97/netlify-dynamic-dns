dcba47afdcdc9c32221e0bea8263a5d38d9ea9ea
TheFatPanda97 <shawnhu97@gmail.com>
2023-08-20 12:41:58 -0400

    added title to website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../scss/global.scss';
-import type { AppProps } from 'next/app';
 import { useState, useEffect } from 'react';
-
+import Head from 'next/head';
 import { ApolloClient, ApolloProvider, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
+
+import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [client, setClient] = useState<ApolloClient<NormalizedCacheObject>>();
@@ -22,6 +23,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ApolloProvider client={client}>
+      <Head>
+        <title>Netlify DDNS</title>
+      </Head>
       <Component {...pageProps} />
     </ApolloProvider>
   );
